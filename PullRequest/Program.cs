91a679335598b21bd1a6f49dbd1b953c01c68d3e
DZM77 <32109758+DZM77@users.noreply.github.com>
2020-06-04 10:18:30 +0200

    Update Program.cs

--- PullRequest/Program.cs
+++ PullRequest/Program.cs
@@ -8,6 +8,7 @@ namespace PullRequest
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+              Console.WriteLine("Hello World!");
         }
     }
 }
